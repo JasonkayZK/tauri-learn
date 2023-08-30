bbbeaccb8fcdf16bcd62676d59408eb6537d4253
jasonkayzk <271226192@qq.com>
2023-08-31 01:22:41 +0800

    finished: frontend call backend

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -6,9 +6,46 @@ fn greet(name: &str) -> String {
     format!("Hello, {}!", name)
 }
 
+#[tauri::command]
+fn error_handle_command() -> Result<String, String> {
+    // If something fails
+    Err("This failed!".into())
+
+    // If it worked
+    // Ok("This worked!".into())
+}
+
+// Return a Result<String, ()> to bypass the borrowing issue
+#[tauri::command]
+async fn async_command(value: &str, bound: i32) -> Result<String, ()> {
+    // Call another async function and wait for it to finish
+    let res = some_async_function(bound).await;
+
+    // Note that the return value must be wrapped in `Ok()` now.
+    Ok(format!("{}, res: {}", value, res))
+}
+
+async fn some_async_function(bound: i32) -> i32 {
+    let mut res = 0;
+    for i in 0..=bound {
+        res += i;
+    }
+    res
+}
+
+#[tauri::command]
+async fn get_window_command(window: tauri::Window) {
+    println!("Window: {}", window.label());
+}
+
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![
+            greet,
+            error_handle_command,
+            async_command,
+            get_window_command,
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
